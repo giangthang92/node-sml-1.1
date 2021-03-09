c53db2ccd0e90951ef2b7c93fc0375e2e5e51c4d
giangthang <shikaku04021992@gmail.com>
2021-03-09 20:34:03 +0700

    baitap

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log('> Hi');
+const express = require('express') // lấy modul express
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Trang Chủ!')
+})
+app.get('/contact',(req, res) => {
+    res.send('Contact!')
+})
+app.get('/chuyen-muc', (req, res) => {
+    res.send('Chuyên mục!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
