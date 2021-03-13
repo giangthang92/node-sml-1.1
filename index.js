0212047bad074d02a32c3863d3f5abaeb9342f13
giangthang <shikaku04021992@gmail.com>
2021-03-13 21:15:22 +0700

    ejs-mate

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const ejsEngine = require('ejs-mate');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use('/public',express.static('./public'));
+app.engine('ejs',ejsEngine);
 
 const port = 3000
 
@@ -13,7 +15,10 @@ const port = 3000
 //     res.redirect("http://example.com");
 // });
 app.get('/', (req, res) => {
-res.render('client/index');
+res.render('client/page/homepage');
+});
+app.get('/post',(req, res) => {
+    res.render('client/page/post');
 });
 
 // app.get('/chuyen-muc/:name',(req, res) => {
