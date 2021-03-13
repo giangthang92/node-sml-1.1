91935d50db91abe8b8000d9bffce370286fa7579
giangthang <shikaku04021992@gmail.com>
2021-03-13 20:11:40 +0700

    edit link

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const app = express();
 const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
-app.set('view', './views');
+app.set('views', './views');
 
 app.use('/public',express.static('./public'));
 
-const port = 3002
+const port = 3000
 
-app.get('/home', (req , res ) => {
-    res.redirect("http://example.com");
-});
-app.get('/chuyen-muc', (req, res) => {
-res.send('<a href="/home">Back home</a> <br> <a href="/chuyen-muc">chuyen fgfg</a>');
+// app.get('/home', (req , res ) => {
+//     res.redirect("http://example.com");
+// });
+app.get('/', (req, res) => {
+res.render('client/index');
 });
 
-app.get('/chuyen-muc/:name',(req, res) => {
-    const postid = req.params.name;
-    res.send('Trang ' + postid);
-})
+// app.get('/chuyen-muc/:name',(req, res) => {
+//     const postid = req.params.name;
+//     res.send('Trang ' + postid);
+// })
 
 app.listen(port,() => {
     console.log('Xử Lý');
-})
+});
